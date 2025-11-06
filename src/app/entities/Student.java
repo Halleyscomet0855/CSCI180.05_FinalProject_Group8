d20c9c42de380e9f48f4bac27e74ea73cfd592d0
Halleyscomet0855 <hunterxhalley@gmail.com>
2025-11-06 19:14:51 +0800

    getters and setters for student: should be all of them

--- src/app/entities/Student.java
+++ src/app/entities/Student.java
@@ -9,14 +9,13 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 
-
 @Entity
 public class Student {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column
-	private Long StudentPK;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column
+  private Long StudentPK;
 
   @Column
   private Long IDNumber;
@@ -27,7 +26,36 @@ public class Student {
   @Column
   private String phoneNumber;
 
-   
-	
-}
+  public Long getStudentPK() {
+    return StudentPK;
+  }
 
+  public void setStudentPK(Long studentPK) {
+    StudentPK = studentPK;
+  }
+
+  public Long getIDNumber() {
+    return IDNumber;
+  }
+
+  public void setIDNumber(Long iDNumber) {
+    IDNumber = iDNumber;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPhoneNumber() {
+    return phoneNumber;
+  }
+
+  public void setPhoneNumber(String phoneNumber) {
+    this.phoneNumber = phoneNumber;
+  }
+
+}
