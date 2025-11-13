1105f177b29411013f351663f9dd27bc018bf15b
Halleyscomet0855 <hunterxhalley@gmail.com>
2025-11-13 13:24:02 +0800

    finished work into simple entities
    not sure how to work on the associative array: more specifically, how
    can we make a composite key in java?

--- src/app/entities/Beadle.java
+++ src/app/entities/Beadle.java
@@ -1,5 +1,58 @@
 package app.entities;
 
-public class Beadle {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Beadle {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Long StudentPK;
+
+	@Column
+	private Long IDNumber;
+
+	@Column
+	private String name;
+
+	@Column
+	private String phoneNumber;
+
+	public Long getStudentPK() {
+		return StudentPK;
+	}
+
+	public void setStudentPK(Long studentPK) {
+		StudentPK = studentPK;
+	}
+
+	public Long getIDNumber() {
+		return IDNumber;
+	}
+
+	public void setIDNumber(Long iDNumber) {
+		IDNumber = iDNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	
 }
--- src/app/entities/Class.java
+++ src/app/entities/Class.java
@@ -1,5 +1,60 @@
 package app.entities;
 
-public class Class {
+import java.sql.Timestamp;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Class {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column
+	private Long ClassPk ;
+	@ManyToOne(fetch = FetchType.EAGER)
+	private Beadle BeadlePk;
+	@Column
+	private String ClassName;
+	@Column
+	private String ProfessorName;
+	@Column
+	private Timestamp Time;
+	
+	public Long getClassPk() {
+		return ClassPk;
+	}
+	public void setClassPk(Long classPk) {
+		ClassPk = classPk;
+	}
+	public Beadle getBeadlePk() {
+		return BeadlePk;
+	}
+	public void setBeadlePk(Beadle beadlePk) {
+		BeadlePk = beadlePk;
+	}
+	public String getClassName() {
+		return ClassName;
+	}
+	public void setClassName(String className) {
+		ClassName = className;
+	}
+	public String getProfessorName() {
+		return ProfessorName;
+	}
+	public void setProfessorName(String professorName) {
+		ProfessorName = professorName;
+	}
+	public Timestamp getTime() {
+		return Time;
+	}
+	public void setTime(Timestamp time) {
+		Time = time;
+	}
+
+	
 }
